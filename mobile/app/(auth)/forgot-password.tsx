14af391036c73b1ca6980f02d06f6ee271d680a0
sahdkhan <03sahd@gmail.com>
2026-03-07 23:55:08 -0800

    Forgot password screen

---
+++ mobile/app/(auth)/forgot-password.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Alert, Text, View } from 'react-native'
+import { theme } from '../../src/ui/theme'
+import { Input } from '../../src/ui/Input'
+import { Button } from '../../src/ui/Button';
+import { api } from '../../src/api/client';
+import { useRouter } from 'expo-router';
+
+export default function ForgotPassword() {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState<string | null>(null);
+
+    const router = useRouter();
+
+    async function handleResetPassword(){
+        setError(null);
+        console.log(email);
+        try {
+            const res = await api<{ message: string }>("/password-reset/reset-password", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email }),
+            });
+            console.log("API RESPONSE:", res);
+            Alert.alert(res.message);
+            router.replace("/login");
+            return;
+        } catch (e: any) {
+            console.log("RESET ERROR:", e);
+            setError(e.message);
+        }
+    }
+
+    return (
+        <View
+          style={{
+            flex: 1,
+            paddingHorizontal: theme.spacing.lg,
+            paddingBottom: theme.spacing.lg,
+            gap: theme.spacing.md,
+            }}
+        >
+            <Text
+            style={{
+                fontSize: theme.textSize.xl,
+                color: theme.colors.text,
+                textAlign: "center",
+            }}
+            >
+              Forgot Password
+            </Text>
+            
+            <Input
+                placeholder="Email"
+                autoCapitalize="none"
+                value={email}
+                onChangeText={setEmail}
+            />  
+
+            <Button
+                label="Reset Password"
+                onPress={handleResetPassword}
+                variant='primary'
+            />
+      </View>
+  )
+}
